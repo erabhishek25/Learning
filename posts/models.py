d5d9f52eac9d170f6eadf61240c69593a8cd1cee
erabhisek <abhishek.er45@gmail.com>
2024-06-21 23:17:29 +0530

    Post app model and migrations

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,22 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+
+class Post(models.Model):
+	"""
+	Blog post information
+	"""
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	author = models.ForeignKey(
+		settings.AUTH_USER_MODEL,
+		on_delete=models.CASCADE
+		)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.title
+
+
